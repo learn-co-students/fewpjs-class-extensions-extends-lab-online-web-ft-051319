db7b787d82e9d2ce2ccfc650b4e9f426623436d1
Natalie <natsetc@gmail.com>
2019-09-08 01:52:37 -0700

    notes

--- index.js
+++ index.js
@@ -21,18 +21,35 @@ class Triangle extends Polygon {
         } else {
             return true;
         }
+
+        // another option
+        // if (!Array.isArray(this.sides)) return;
+        // if (this.count !== 3) return;
+        // let side1 = this.sides[0]
+        // let side2 = this.sides[1]
+        // let side3 = this.sides[2]
+        // return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
     }
 }
 
 class Square extends Polygon {
     get isValid() {
-        for (let i = 0; i < this.sides.length; i++) {
-            if (this.sides[i] !== this.sides[i+1]) {
-                return false;
-            } else {
-                return true;
-            }
-        }
+        // better option
+        // if (!Array.isArray(this.sides)) return;
+        // if (this.count !== 4) return;
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        let side4 = this.sides[3]
+        return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+
+        // for (let i = 0; i < this.sides.length; i++) {
+        //     if (this.sides[i] !== this.sides[i+1]) {
+        //         return false;
+        //     } else {
+        //         return true;
+        //     }
+        // }
     }
 
     get area() {
